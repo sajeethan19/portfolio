e2ce3924aa86c198842249e873a78d3639c33886
Sajeethan Gulendran <sajeethan@smartzi.com>
2026-03-29 12:55:52 +0530

    add: gh page configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS === "true";
+const repositoryName = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "";
+const isUserOrOrgSite = repositoryName.endsWith(".github.io");
+const basePath = isGithubActions && !isUserOrOrgSite ? `/${repositoryName}` : "";
+
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  basePath,
+  assetPrefix: basePath ? `${basePath}/` : undefined
 };
 
 export default nextConfig;
